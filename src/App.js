6a08efa4ec4b56895d6e69ea9c3375c7953a6ca0
matan0810 <65176459+matan0810@users.noreply.github.com>
2022-09-24 22:42:10 +0300

    add safer reset link button

--- src/App.js
+++ src/App.js
@@ -2,37 +2,50 @@ import { useEffect, useState } from "react";
 import VideoPlayer from "./VideoPlayer/VideoPlayer";
 import "./App.css";
 
-// https://es1.quest123.top:8088/live/socolivepro10010/playlist.m3u8?vidictid=192491573324&id=116133&pk=7b31a258377513b27d9e69323074e8702ca4fd2310572030149f4b7ba0faa635d4add3793553a011c1594b260ca8644a0d2040b424f2b622e7d2f0112c72ee07
-
 export default function App() {
   const [link, setLink] = useState(localStorage.getItem("link"));
-  const [field, setField] = useState();
 
   useEffect(() => {
-    localStorage.setItem("link", field);
-  }, [field]);
+    if (link) localStorage.setItem("link", link);
+  }, [link]);
 
   return (
     <div className="App">
-      <div className="body">
-        <div className="form">
-          <form onSubmit={(e) => e.preventDefault()}>
-            <label>הכנס קישור:</label>
-            <input
-              className="input"
-              type="text"
-              required
-              value={field}
-              onChange={({ target }) => setField(target.value)}
-            />
-            <button className="button" onClick={() => setLink(field)}>
-              בחר
-            </button>
-          </form>
-        </div>
+      <VideoPlayer cameraLink={link} />
+      <Form link={link} setLink={setLink} />
+    </div>
+  );
+}
 
-        <VideoPlayer cameraLink={link} />
-      </div>
+function Form({ link, setLink }) {
+  const [field, setField] = useState(link);
+
+  return (
+    <div className="form">
+      <form onSubmit={(e) => e.preventDefault()}>
+        <label>הכנס קישור:</label>
+        <input
+          className="input"
+          type="text"
+          required
+          disabled={!!link}
+          value={field}
+          onChange={({ target }) => setField(target.value)}
+        />
+        {link ? (
+          <button
+            className="button"
+            style={{ backgroundColor: "orange" }}
+            onClick={() => setLink("")}
+          >
+            בחר מחדש
+          </button>
+        ) : (
+          <button className="button" onClick={() => setLink(field)}>
+            בחר
+          </button>
+        )}
+      </form>
     </div>
   );
 }
--- src/VideoPlayer/VideoPlayer.js
+++ src/VideoPlayer/VideoPlayer.js
@@ -33,7 +33,13 @@ export default function VideoPlayer({ cameraLink }) {
   if (!cameraLink) return null;
 
   return (
-    <div data-vjs-player>
+    <div
+      style={{
+        boxShadow:
+          "rgba(0, 0, 0, 0.4) 0px 2px 4px, rgba(0, 0, 0, 0.3) 0px 7px 13px -3px, rgba(0, 0, 0, 0.2) 0px -3px 0px inset",
+      }}
+      data-vjs-player
+    >
       <video ref={setVideoNode} className="video-js" />
     </div>
   );
