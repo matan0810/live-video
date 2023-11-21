8d2875ddd79c975ba77e5cb2c1e3b698dd509165
matan0810 <65176459+matan0810@users.noreply.github.com>
2023-11-22 00:10:46 +0200

    update links

--- src/SelectLink/links.js
+++ src/SelectLink/links.js
@@ -1,11 +1,9 @@
 export const links = {
   Kan11:
     "https://kan11w.media.kan.org.il/hls/live/2105694/2105694/source1_600/chunklist.m3u8",
-  N12: "https://mako-streaming.akamaized.net/stream/hls/live/2033791/k12dvr/index.m3u8?b-in-range=0-1100&_uid=W2FB67B4B25CC-61E7-9B1D-B486553A2246&playerSessionid=fe323981-61ab-4dd3-abb7-27782f03a28a&hdnea=st%3D1698755623%7Eexp%3D1698756523%7Eacl%3D%2F*%7Ehmac%3D12344c60a7b664bf2bab66101d54c643542622722538aef47d8a34a542ca7069",
   N13: "https://reshet.g-mana.live/media/87f59c77-03f6-4bad-a648-897e095e7360/profile/2/profileManifest.m3u8?_uid=6beb1aa8-7748-4dc3-9747-7d28536688bd&rK=a2&_did=16404bd7b2927f9d2b4d9f582376c8724a33f112",
   Now14:
     "https://hub7.g-mana.live/media/91517161-44ab-4e46-af70-e9fe26117d2e/profile/2/profileManifest.m3u8?_uid=43cfd941-0f1c-4d75-8dfc-965a3964b64b&rK=a2&_did=16404bd7b2927f9d2b4d9f582376c8724a33f112",
   CNN: "https://d2lckchr9cxrss.cloudfront.net/out/v1/c73af7694cce4767888c08a7534b503c/index.m3u8",
-  SKY_NEWS:
-    "https://linear021-gb-hls1-prd-ak.cdn.skycdp.com/Content/HLS_001_sd/Live/channel(skynews)/04_hd30.m3u8",
+  CBS: "https://dai.google.com/linear/hls/pa/event/Sid4xiTQTkCT1SLu6rjUSQ/stream/71e85065-d444-4640-80c8-68be8726c398:BRU/variant/9d102376758f0f0f4b40bf244f370f11/bandwidth/1093281.m3u8",
 };
