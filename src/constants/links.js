73960c14e55fb44fa17b4f9384c986f2cde82e77
matan0810 <65176459+matan0810@users.noreply.github.com>
2024-11-05 12:41:17 +0200

    update links

--- src/constants/links.js
+++ src/constants/links.js
@@ -1,34 +1,37 @@
 export const links = {
   Kan11:
     "https://kan11w.media.kan.org.il/hls/live/2105694/2105694/source1_600/chunklist.m3u8",
-  N12: {
-    link: "https://nirozbdikot.blogspot.com/p/keshet.html",
-    isIFrame: true,
-  },
-  N13: "https://reshet.g-mana.live/media/87f59c77-03f6-4bad-a648-897e095e7360/profile/2/profileManifest.m3u8?_uid=6beb1aa8-7748-4dc3-9747-7d28536688bd&rK=a2&_did=16404bd7b2927f9d2b4d9f582376c8724a33f112",
+  // N12: {
+  //   link: "https://nirozbdikot.blogspot.com/p/keshet.html",
+  //   isIFrame: true,
+  // },
+  Reshet13:
+    "https://reshet.g-mana.live/media/87f59c77-03f6-4bad-a648-897e095e7360/profile/2/profileManifest.m3u8?_uid=6beb1aa8-7748-4dc3-9747-7d28536688bd&rK=a2&_did=16404bd7b2927f9d2b4d9f582376c8724a33f112",
   Now14:
-    "https://hub7.g-mana.live/media/91517161-44ab-4e46-af70-e9fe26117d2e/profile/2/profileManifest.m3u8?_uid=43cfd941-0f1c-4d75-8dfc-965a3964b64b&rK=a2&_did=16404bd7b2927f9d2b4d9f582376c8724a33f112",
-  Ynet: "https://ynet-live-01.ynet-pic1.yit.co.il/ynet/live_720/index.m3u8",
+    "https://ch14-channel14-content.akamaized.net/hls/live/2104807/CH14_CHANNEL14/2/streamPlaylist.m3u8",
+  // Ynet: "https://ynet-live-01.ynet-pic1.yit.co.il/ynet/live_720/index.m3u8",
   Walla:
     "https://cdn-uw2-prod.tsv2.amagi.tv/linear/amg01742-walla-wallanews-ono/playlist.m3u8",
-  תשע: "https://contactgbs.mmdlive.lldns.net/contactgbs/dd4f5f04932345f1b47c4bfb45fbd682/chunklist_b1128000.m3u8",
+  // תשע: "https://contactgbs.mmdlive.lldns.net/contactgbs/dd4f5f04932345f1b47c4bfb45fbd682/chunklist_b1128000.m3u8",
   חינוכית:
     "https://kan23.media.kan.org.il/hls/live/2024691/2024691/master.m3u8",
   Knesset:
     "https://contact.gostreaming.tv/Knesset/myStream/chunklist_w882959991.m3u8",
   Makkan:
     "https://makan.media.kan.org.il/hls/live/2024680/2024680/source1_4k/chunklist.m3u8",
-  "ערוץ הקניות":
-    "https://shoppingil-rewriter.vidnt.com/video_10801920_p_0.m3u8",
+  // "ערוץ הקניות":
+  //   "https://shoppingil-rewriter.vidnt.com/video_10801920_p_0.m3u8",
+  "ערוץ הקומדיות":
+    "https://d15ds134q59udk.cloudfront.net/media/3b45de9e-c944-4fdc-a344-7c3042b5b104/profile/1/profileManifest.m3u8?_uid=942a06d5-6c6d-42d3-a980-cd7e46ebb2e7&rK=a2&_did=d08df654dedf5609e7d0943be084337edb50cdd7",
   CNN: "https://d2lckchr9cxrss.cloudfront.net/out/v1/c73af7694cce4767888c08a7534b503c/index.m3u8",
-  CNNi: "https://cnn-cnninternational-1-eu.rakuten.wurl.tv/f93b9a949a4a8651dd5a2f42e3a2b6f9.m3u8",
+  // CNNi: "https://cnn-cnninternational-1-eu.rakuten.wurl.tv/f93b9a949a4a8651dd5a2f42e3a2b6f9.m3u8",
   NBC: "https://d2gjhy8g9ziabr.cloudfront.net/v1/master/3fec3e5cac39a52b2132f9c66c83dae043dc17d4/prod-samsungtvplus-stitched/samsungtvplus_us_nbcnewsnow_001.m3u8",
   FOX_NEWS:
     "https://247preview.foxnews.com/hls/live/2020027/fncv3preview/index.m3u8",
   ABC: "https://content.uplynk.com/channel/3324f2467c414329b3b0cc5cd987b6be.m3u8",
   CBS: "https://cbsn-us.cbsnstream.cbsnews.com/out/v1/55a8648e8f134e82a470f83d562deeca/master.m3u8",
   Sky_News:
-    "https://linear021-gb-hls1-prd-ak.cdn.skycdp.com/Content/HLS_001_sd/Live/channel(skynews)/08_hd.m3u8",
+    "https://linear417-gb-hls1-prd-ak.cdn.skycdp.com/100e/Content/HLS_001_1080_30/Live/channel(skynews)/03_1080-30.m3u8",
   France24:
     "https://cdn.klowdtv.net/803B48A/n1.klowdtv.net/live2/france24_720p/chunks.m3u8",
   DW: "https://dwamdstream102.akamaized.net/hls/live/2015525/dwstream102/stream05/streamPlaylist.m3u8",
